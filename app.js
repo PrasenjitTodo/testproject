3c0b9972c686788dad43a183de6c3dca08a3186b
Prasenjit <prasenjit.ttdooo@gmail.com>
2024-02-09 17:01:17 +0530

    Test

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/', (req, res, next)=>{
     
 
     console.log('-: Welcome :-');
-    res.send('-: Welcome :-');
+    res.send('-: Welcome :- Msg : '+message+' CryptoJS Hash : '+ciphertext.toString()+' DeCrypt Msg : '+plaintext);
     next();
 });
 
